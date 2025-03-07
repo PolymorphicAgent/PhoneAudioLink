9502904c297302d7c82e371ce85252aeaf010289
PolymorphicAgent <polymorphicagent07@gmail.com>
2025-03-07 09:13:48 -0500

    more testing

--- a2dpstreamer.cpp
+++ a2dpstreamer.cpp
@@ -1,5 +1,48 @@
 #include "a2dpstreamer.h"
+#include "qdebug.h"
 
-A2DPStreamer::A2DPStreamer(QObject *parent)
-    : QObject{parent}
-{}
+A2DPStreamer::A2DPStreamer(QObject* parent)
+    : QObject(parent), pipeline(nullptr)
+{
+    gst_init(nullptr, nullptr);
+}
+
+A2DPStreamer::~A2DPStreamer() {
+    if (pipeline)
+        gst_object_unref(pipeline);
+}
+
+// Initialize the pipeline with elements suited for A2DP.
+// Note: The exact pipeline will depend on your target platform
+// and available plugins. For instance, on Linux you might use
+// the "bluez_a2dp_sink" element with BlueZ.
+bool A2DPStreamer::setupPipeline(const QString& deviceAddress) {
+    // Build your pipeline string using the deviceAddress.
+    // This is a simplified example:
+    QString pipelineStr = QString("audiotestsrc ! audioconvert ! audioresample ! "
+                                  "wasapisink device=%1")
+                              .arg(deviceAddress);
+    GError* error = nullptr;
+    pipeline = gst_parse_launch(pipelineStr.toUtf8().constData(), &error);
+    if (!pipeline) {
+        qWarning() << "Failed to create pipeline:" << error->message;
+        g_error_free(error);
+        return false;
+    }
+    return true;
+}
+
+void A2DPStreamer::play() {
+    if (pipeline)
+        gst_element_set_state(pipeline, GST_STATE_PLAYING);
+}
+
+void A2DPStreamer::pause() {
+    if (pipeline)
+        gst_element_set_state(pipeline, GST_STATE_PAUSED);
+}
+
+void A2DPStreamer::stop() {
+    if (pipeline)
+        gst_element_set_state(pipeline, GST_STATE_NULL);
+}
--- a2dpstreamer.h
+++ a2dpstreamer.h
@@ -10,8 +10,21 @@ class A2DPStreamer : public QObject
     Q_OBJECT
 public:
     explicit A2DPStreamer(QObject *parent = nullptr);
+    ~A2DPStreamer();
+
+    bool setupPipeline(const QString&);
+
+public slots:
+    void play();
+    void pause();
+    void stop();
 
 signals:
+    void connectionStatusChanged(bool);
+    // Additional signals for error reporting, etc.
+
+private:
+    GstElement* pipeline;
 };
 
 #endif // A2DPSTREAMER_H
