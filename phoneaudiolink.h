058d7c62fc4cbc4a8bd1537b33357dd2284abd68
PolymorphicAgent <polymorphicagent07@gmail.com>
2025-03-10 13:55:34 -0400

    add comments

--- phoneaudiolink.h
+++ phoneaudiolink.h
@@ -2,6 +2,7 @@
 #define PHONEAUDIOLINK_H
 
 #include "startuphelp.h"
+#include "A2DPStreamer.h"
 
 #include <QBluetoothDeviceDiscoveryAgent>
 #include <QBluetoothLocalDevice>
@@ -13,6 +14,7 @@
 #include <QJsonArray>
 #include <QToolTip>
 #include <QFile>
+#include <QList>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -28,6 +30,7 @@ public:
     PhoneAudioLink(QWidget *parent = nullptr);
     ~PhoneAudioLink();
 
+    //returns whether or not the program should be starting minimized
     bool getStartMinimized();
 
 protected:
@@ -35,27 +38,30 @@ protected:
     void changeEvent(QEvent *event) override; //triggers when the app is minimized
 
 private slots:
-    void playPause();
-    void startDiscovery();
-    void appendDevice(const QBluetoothDeviceInfo &);
-    void connectSelectedDevice();
-    void deviceComboChanged(int);
+    void playPause();//is triggered when the play/pause button is pressed
+    void startDiscovery();//starts the automatic discovery of bluetooth devices.
+    void appendDevice(const QBluetoothDeviceInfo &);//is connected to the bluetooth discovery agent's deviceDiscovered slot
+    void connectSelectedDevice(); //triggers when the "connect" button is pressed
+    void deviceComboChanged(int); //triggers when the deviceComboBox's index changes
 
-    void saveInitData();
-    void loadInitData();
-    void showFromTray();
-    void exitApp();
+    void saveInitData();//saves the json initialization configuration
+    void loadInitData();//loads the json initialization configuration
+    void showFromTray();//show the app from tray
+    void exitApp();//saves init data, then exits the app
 
 private:
-    StartupHelp *startupHelp;
-    Ui::PhoneAudioLink *ui;
-    QBluetoothDeviceDiscoveryAgent *discoveryAgent;
-    QBluetoothAddress savedDeviceAddress;
-    QSystemTrayIcon *trayIcon;
-    QMenu *trayMenu;
-    bool maximizeBluetoothCompatability, startMinimized, connectAutomatically;
+    StartupHelp *startupHelp; //declare startup help dialog (instructs user on how to add the program to startup)
+    Ui::PhoneAudioLink *ui; //the ui
+    QBluetoothDeviceDiscoveryAgent *discoveryAgent; //the discovety agent
+    QBluetoothAddress savedDeviceAddress; //the address of the device from our json initialization configuration file
+    QSystemTrayIcon *trayIcon; //the tray icon
+    QMenu *trayMenu; //the tray context menu
+    bool maximizeBluetoothCompatability, startMinimized, connectAutomatically;//these are loaded from our json initialization configuration file
 
-    QString stringifyUuids(QList<QBluetoothUuid>);
+    QString stringifyUuids(QList<QBluetoothUuid>); //for debugging purposes
 
+    //members for A2DP streaming
+    A2DPStreamer *a2dpStreamer; //our streaming handler
+    QList<QBluetoothDeviceInfo> discoveredDevices; //list of discovered devices
 };
 #endif // PHONEAUDIOLINK_H
